7c3469694f5e106a24a6778a1cbdde1bf789b344
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-04-26 15:52:00 -0400

    Este es le primer commit dede intellij

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        primer p = new primer();
+        p.sumar(1,2);
     }
 }
---
+++ src/primer.java
@@ -0,0 +1,6 @@
+public class primer {
+
+    public void sumar (int a , int b){
+        System.out.println(a+b);
+    }
+}
